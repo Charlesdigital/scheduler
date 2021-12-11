e5b5aedb150d0cb334497eb40784b048024896f7
Charlesdigital <charles.easton123@gmail.com>
2021-12-11 17:39:24 -0500

    currentComponent function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 function index(props) {
+  const currentComponent = function (interview) {
+    if (interview) {
+      return <Show />;
+    } else {
+      return <Empty />;
+    }
+  };
   return (
-    <article className="appointment">Appointment at {props.time} </article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {currentComponent(props.interview)}
+    </article>
   );
 }
 
