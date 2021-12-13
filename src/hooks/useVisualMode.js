2909580e8caae96b9308c3447f900c5c5b9f170e
Charlesdigital <charles.easton123@gmail.com>
2021-12-13 15:54:45 -0500

    add  retrieving Interviewer Data tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const updatedHistory = [...history];
+
+  const transition = (mode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, mode]);
+    }
+    return setMode(mode);
+  };
+
+  const back = () => {
+    if (updatedHistory.length > 1) {
+      updatedHistory.pop();
+      setHistory(updatedHistory);
+      return setMode(updatedHistory[updatedHistory.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
+}
